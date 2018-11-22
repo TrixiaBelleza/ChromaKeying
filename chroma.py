dbcf82b3a68628e08f751cfcd475d28ee6048b40
TriziaLeal <mmleal@up.edu.ph>
2018-11-23 00:22:43 +0800

    Fixed problem when foreground video is resized

--- chroma.py
+++ chroma.py
@@ -1,14 +1,11 @@
 import cv2
 import numpy as np 
 
-background = "bg"
-foreground = "harot"
-
+background = "trix_turn_around_long"
+foreground = "fg_trix_turn_around_crop"
 
 cap = cv2.VideoCapture("./foreground/" + foreground + '.mp4')
 b = cv2.VideoCapture("./background/" + background + '.mp4') 
-
-
 
 font                   = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,500)
@@ -16,8 +13,6 @@ fontScale              = 1
 fontColor              = (0,0,255)
 lineType               = 2
 flag                   = 0
-
-
 
 width = int(b.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(b.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -27,19 +22,19 @@ fourcc = cv2.VideoWriter_fourcc('X','V','I','D') # four-char-code
 frames_per_second = 20.0
 out = cv2.VideoWriter('result_' + foreground + '.avi', fourcc, frames_per_second, dimension)
 
-
-
 while(cap.isOpened()):
   ret, frame = cap.read()
   ret2, frame2 = b.read()
 
+  frame = cv2.resize(frame,(width,height))
   #Adjust niu to
-  lower_green = np.array([20,60,60])
-  upper_green = np.array([100,255,255])
+  lower_green = np.array([40,60,60])
+  upper_green = np.array([90,255,255])
 
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # convert input to gray scale
   mask = cv2.inRange(hsv, lower_green, upper_green)
   mask_inv = cv2.bitwise_not(mask)
+
 
 
   green = cv2.bitwise_and(frame,frame, mask = mask)
